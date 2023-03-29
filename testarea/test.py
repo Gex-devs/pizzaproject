f7d0d97adcb9feecb876267401d639d32507abae
Gexion <gex@localhost.localdomain>
2023-03-29 19:38:38 +0200

    setup local env

--- testarea/test.py
+++ testarea/test.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template,jsonify
 from flask_mysqldb import MySQL
-import time
+import time,os
+from dotenv import load_dotenv
+
+# load local env
+load_dotenv()
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'Gex'
-app.config['MYSQL_PASSWORD'] = 'test123'
-app.config['MYSQL_DB'] = 'trial'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+
 
 mysql = MySQL(app)
 
