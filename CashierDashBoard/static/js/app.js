d1c18f46f8c01e36c77fd69c5f02222704348ed4
Gex-dev69 <dgexion@outlook.com>
2023-04-08 21:03:21 +0300

    Fixing order pender

--- CashierDashBoard/static/js/app.js
+++ CashierDashBoard/static/js/app.js
@@ -7,13 +7,13 @@ async function addToBucket(button) {
         inputClass: 'text-center',
         inputValue: inputValue,
         showCancelButton: true,
-        inputAttributes:{
+        inputAttributes: {
             style: 'width: 80px;postion:relative;margin-left:40%',
         },
         inputValidator: (value) => {
             if (!value) {
                 return 'You need to write something!'
-            }else{
+            } else {
                 inputValue = value
             }
         }
@@ -22,6 +22,32 @@ async function addToBucket(button) {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'http://127.0.0.1:5000/addToBucket', true);
     xhr.setRequestHeader('Content-Type', 'text/plain');
-    var data = button.value+":"+inputValue;
+    var data = button.value + ":" + inputValue;
     xhr.send(data);
+}
+
+
+function addOrder() {
+
+}
+function sendOrder() {
+    Swal.fire({
+        title: 'Did you add all orders?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            const insideItems = document.querySelectorAll('.InsideItems');
+            insideItems.forEach(item => {
+                const classifier = item.querySelector('.InsideItemsClassifier');
+                const itemName = classifier.children[0].textContent;
+                const itemQuantity = classifier.children[1].textContent;
+                console.log(itemName, itemQuantity);
+            });
+
+        }
+    })
 }
